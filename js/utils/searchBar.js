cfcc661a58b68ab57b40791f449e9bf232b4c2f5
jeremy twardowski <kherset@protonmail.com>
2023-10-28 16:50:23 +0200

    error

--- js/utils/searchBar.js
+++ js/utils/searchBar.js
@@ -13,10 +13,11 @@ async function searchRecipe() {
 
 // Create a list of recipes based on the data
 function createResearchList(recipes) {
-  recipes.forEach(recipe => {
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
     let recipeCard = cardTemplate(recipe);
     resultsList.appendChild(recipeCard);
-  });
+  }
 }
 
 // Add an event listener to the search input for filtering data
@@ -32,28 +33,34 @@ function filterData(e) {
     let filteredData = searchInsideRecipes(recipes, searchedName);
     displayNumberOfRecipes(filteredData);
   } else {
-    numberOfRecipes.innerText = `${recipes.length} recipes`;
+    numberOfRecipes.innerText = `${recipes.length} recettes`;
     createResearchList(recipes);
   }
 }
 
 // Search for keywords inside recipes
 function searchInsideRecipes(dataBase, dataResearched) {
-  const filteredData = dataBase.filter(element =>
-    element.name.toLowerCase().includes(dataResearched) ||
-    element.description.toLowerCase().includes(dataResearched) ||
-    element.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(dataResearched))
-  );
+  const filteredData = [];
+  for (let i = 0; i < dataBase.length; i++) {
+    const element = dataBase[i];
+    if (
+      element.name.toLowerCase().includes(dataResearched) ||
+      element.description.toLowerCase().includes(dataResearched) ||
+      element.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(dataResearched))
+    ) {
+      filteredData.push(element);
+    }
+  }
   return filteredData;
 }
 
 // Display the number of recipes and update the list
 function displayNumberOfRecipes(filteredData) {
-  if (filteredData.length === 1) {
-    numberOfRecipes.innerText = `${filteredData.length} recipe`;
-    createResearchList(filteredData);
+  const numRecipes = filteredData.length;
+  if (numRecipes === 1) {
+    numberOfRecipes.innerText = `${numRecipes} recette`;
   } else {
-    numberOfRecipes.innerText = `${filteredData.length} recipes`;
-    createResearchList(filteredData);
+    numberOfRecipes.innerText = `${numRecipes} recettes`;
   }
+  createResearchList(filteredData);
 }
